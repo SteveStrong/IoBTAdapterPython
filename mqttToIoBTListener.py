63a7981cab8121312b170f396ebcd02f8c4cc861
Greg Fulton <gtfullton@gmail.com>
2021-08-13 08:29:01 -0400

    disconnect mqtt on exit from application

--- mqttToIoBTListener.py
+++ mqttToIoBTListener.py
@@ -1,5 +1,8 @@
+import sys
+import signal
 from .wrappers.MQTTWrapper import MQTTtoIoBTWrapper
 from .iobtServerRealtime import ClientHubConnector
+
 
 mqttBroker = "demo.iobtlab.com"
 username = "techworks"
@@ -18,8 +21,14 @@ def main():
 
     mqttHub.do_loop()
 
+    def end_of_processing(signal_number, stack_frame):
+        print(f"Exiting")
+        mqttHub.client.disconnect()
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, end_of_processing)
     # cross-platform way to pause python execution.  signal is not available in windows
-    input("Press the <Enter> key or <ctrl-C> to continue...")
+    input("\nPress the <Enter> key or <ctrl-C> to continue...\n\n")
 
 
 # if __name__ == '__main__':
