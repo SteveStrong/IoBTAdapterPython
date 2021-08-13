ff8a48317ab9a771698244d9bb61b196898ccc2d
Greg Fulton <gtfullton@gmail.com>
2021-08-13 08:29:36 -0400

    Try to get conda to work in docker; it's a strange beast

--- conda_install.sh
+++ conda_install.sh
@@ -1,28 +1,12 @@
 #!/bin/bash
-
-conda create --name iobt-adapter python=3.8
+export PATH=~/miniconda/bin:$PATH
+conda create -y --name iobt-adapter python=3.8
 conda install -c conda-forge opencv
 conda install -c conda-forge paho-mqtt
 conda install -c conda-forge numpy
-# conda install -c conda-forge pysimplegui
 conda install -c conda-forge imuteils
-
 conda install -c anaconda pandas
-
 conda install -c conda-forge wxpython
 conda install -c conda-forge rx
-
-# conda install -c conda-forge moviepy
-# conda install -c pytorch pytorchy
-# conda install -c pytorch torchvisiony
-# conda install -c conda-forge cupy
-
-# conda install -c conda-forge onnx
-# conda install -c conda-forge onnxruntime
-
-# to integrate bash with conda
-# conda init bash  
-
-conda activate iobt-adapter
 pip install SoundCard
 pip install signalrcore
